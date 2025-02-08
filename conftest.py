454f525b4e9033ebb72c142cbb7523a607a06a23
chayko-anna <al.nchko@gmail.com>
2025-02-09 00:14:58 +0300

    fix conftest

--- conftest.py
+++ conftest.py
@@ -1,17 +1,32 @@
 import pytest
 from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_settings():
-    browser.config.driver_options = webdriver.ChromeOptions()
-    #browser.config.driver_options.page_load_strategy = 'eager'
-    #browser.config.driver_options.add_argument("--headless")
-    #browser.config.base_url = "https://demoqa.com"
-    browser.driver.maximize_window()
+    options = Options()
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
 
-    yield
+    options.capabilities.update(capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
+    #browser.config.driver_options = webdriver.ChromeOptions()
+    #browser.driver.maximize_window()
+
+    yield browser
 
     browser.quit()
